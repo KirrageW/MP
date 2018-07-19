38fa0a62631a1dfec1b9fe99359a2794a9f80a5b
unknown <2354535k@student.gla.ac.uk>
2018-07-19 11:58:07 +0100

    working on collisions

--- Globe.java
+++ Globe.java
@@ -12,9 +12,6 @@ public class Globe {
 	private int[][] groupMap; // this will be a 2d array map that can check for the presence of two Squares in
 								// the same place
 
-	private ArrayList<Square> group1;
-	private ArrayList<Square> group2;
-
 	private ArrayList<Square>[] continents;
 	private int continentsCounter;
 
@@ -37,18 +34,18 @@ public class Globe {
 		}
 
 		// new 2d array which will take coordinates from Squares, and store their height
-		setHeightMap(new int[size][size]);
+		setHeightMap(new int[size + 1][size + 1]);
 
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
+		for (int i = 0; i < size + 1; i++) {
+			for (int j = 0; j < size + 1; j++) {
 				getHeightMap()[i][j] = -1;
 			}
 		}
 
 		// new 2d array which takes coordinates from Squares, and stores their group
-		groupMap = new int[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
+		groupMap = new int[size + 1][size + 1];
+		for (int i = 0; i < size + 1; i++) {
+			for (int j = 0; j < size + 1; j++) {
 				groupMap[i][j] = 0;
 			}
 		}
@@ -56,9 +53,6 @@ public class Globe {
 
 	// **********************************************************************************************************
 	// TO DO:
-
-	// MAKE ISLAND GENERATOR - AUTO GROUP TOO - use the 2d int array, generate
-	// random 1s, then group to make islands. internet search for it.
 
 	// better collisions
 
@@ -78,56 +72,6 @@ public class Globe {
 
 	// **********************************************************************************************************
 
-	// temp
-	public void makeTestContinent() {
-
-		group1 = new ArrayList<Square>();
-
-		for (int i = size / 4; i < size / 3; i++) {
-			for (int j = size / 4; j < size / 2; j++) {
-				squares[i][j].setX(i);
-				squares[i][j].setY(j);
-				squares[i][j].setHeight(150);
-				squares[i][j].setGroup(1);
-
-				group1.add(squares[i][j]);
-
-			}
-		}
-		continents[continentsCounter] = group1;
-		continentsCounter++;
-	}
-
-	// start to make an int array map to define islands
-	// can sys.out it to view it easily.
-
-	// generate random numbers , 1s, 2s, 3s depedning on number of initial
-	// continents you want
-	// sea is -1
-
-	// then do things with finding neighbours and such
-
-	public void generate(int x) {
-		Random r = new Random();
-		for (int i = 0; i < size * 2; i++) {
-
-			int e = r.nextInt(size);
-			int f = r.nextInt(size);
-
-			groupMap[e][f] = 1;
-
-		}
-
-		for (int i = 0; i < size; i++) {
-			System.out.println("");
-
-			for (int j = 0; j < size; j++) {
-				System.out.print(groupMap[i][j]);
-			}
-		}
-
-	}
-
 	// make array of pointers to grid
 	// assign first N of them IDs - 1, 2, 3, etc
 	// until array points to no spaces that doo not have IDs
@@ -142,19 +86,19 @@ public class Globe {
 	// iterate over the 2d array smd insert part ID numbers in the empty cells based
 	// on the contents of their surrounding cells
 	// let them expand and fight it out -- I can just take one ID, or two IDs etc.
-	
-	
-	// now make this happen in a certain area of the total area, and translate it to real square coordinates, and THEN reset the group map
+
+	// now make this happen in a certain area of the total area, and translate it to
+	// real square coordinates, and THEN reset the group map
 	// group map should reset normally if plot method occurs after the generation...
-	
+
 	// need to use it all to make different groups
 
 	public void newNumbers(int x, int y, int size, int group) {
 		ArrayList<Square> cont = new ArrayList<Square>();
 		Random ran = new Random();
 
-		for (int i = x+2; i < size - 2; i++) {
-			for (int j = y+2; j < size - 2; j++) {
+		for (int i = x + 2; i < size - 2; i++) {
+			for (int j = y + 2; j < size - 2; j++) {
 				int number = ran.nextInt(4);
 				groupMap[i][j] = number;
 			}
@@ -174,20 +118,17 @@ public class Globe {
 		closer();
 		focus();
 
-		
 		// **************************************************************** VIEW NUMBERS
-	/*	for (int i = 1; i < this.size - 1; i++) {
-			System.out.println();
-
-			for (int j = 1; j < this.size - 1; j++) {
-				System.out.print(groupMap[i][j] + " ");
-			}
-		}*/
+		/*
+		 * for (int i = 1; i < this.size - 1; i++) { System.out.println();
+		 * 
+		 * for (int j = 1; j < this.size - 1; j++) { System.out.print(groupMap[i][j] +
+		 * " "); } }
+		 */
 		// *********************************************************************
-		
-		
-		for (int i = 2; i < this.size - 2; i++) {
-			for (int j = 2; j < this.size - 2; j++) {
+
+		for (int i = 0; i < this.size; i++) {
+			for (int j = 0; j < this.size; j++) {
 				if (groupMap[i][j] == 1) {
 					squares[i][j].setX(i);
 					squares[i][j].setY(j);
@@ -199,12 +140,18 @@ public class Globe {
 				}
 			}
 		}
-			continents[continentsCounter] = cont;
-			continentsCounter++;
-			
-			plotToHeightMap();
-			plotToGroupMap();
-		
+
+		continents[continentsCounter] = cont;
+		continentsCounter++;
+
+		System.err.println(continentsCounter + " " + continents[continentsCounter - 1].size());
+
+		plotToHeightMap();
+		plotToGroupMap();
+
+		// is it counting them all as same group
+		// is it rounding up to 1.
+
 	}
 
 	public int getNeighbours(int i, int j) {
@@ -275,11 +222,11 @@ public class Globe {
 		}
 
 	}
-	
+
 	// gets rid of lakes
 	public void focus() {
-		for (int i = 1; i < size-1; i++) {
-			for (int j = 1; j < size-1; j++) {
+		for (int i = 1; i < size - 1; i++) {
+			for (int j = 1; j < size - 1; j++) {
 				int numberOfNeighbours = 0;
 				int req = 3;
 				if (groupMap[i][j] == 0) {
@@ -307,7 +254,7 @@ public class Globe {
 					if (groupMap[i - 1][j] > 0) {
 						numberOfNeighbours++;
 					}
-					
+
 					if (numberOfNeighbours > req) {
 						groupMap[i][j] = 1;
 
@@ -315,11 +262,9 @@ public class Globe {
 				}
 			}
 		}
-	
-			
-				
+
 	}
-	
+
 	public void closer1() {
 
 		for (int i = 0; i < size; i++) {
@@ -365,26 +310,6 @@ public class Globe {
 
 	}
 
-	// temp
-	public void makeOtherContinent() {
-
-		group2 = new ArrayList<Square>();
-
-		for (int i = size / 4 + size / 4; i < size / 3 + size / 4; i++) {
-			for (int j = size / 4; j < size / 3; j++) {
-				squares[i][j].setX(i);
-				squares[i][j].setY(j);
-				squares[i][j].setHeight(150);
-				squares[i][j].setGroup(2);
-
-				group2.add(squares[i][j]);
-			}
-		}
-
-		continents[continentsCounter] = group2;
-		continentsCounter++;
-	}
-
 	// sets velocities of all members of chosen group
 	public void setVelocity(int group, int xVel, int yVel) {
 		for (int i = 0; i < size; i++) {
@@ -425,6 +350,23 @@ public class Globe {
 					squares[i][j].setY(amountUnder);
 				}
 
+				// repeat again as it could have gone above or below for the X AND the Y
+				// coordinate, at a corner
+				if (squares[i][j].getX() + squares[i][j].getXVel() >= size) {
+					// find the amount it's gone over the limit, don't just set to 0
+					int amountOver = (squares[i][j].getX() + squares[i][j].getXVel()) - size;
+					squares[i][j].setX(amountOver);
+				} else if (squares[i][j].getY() + squares[i][j].getYVel() >= size) {
+					int amountOver = (squares[i][j].getY() + squares[i][j].getYVel()) - size;
+					squares[i][j].setY(amountOver);
+				} else if (squares[i][j].getX() + squares[i][j].getXVel() < 0) {
+					// find the amount it's gone under 0, set to size - that
+					int amountUnder = squares[i][j].getX() + squares[i][j].getXVel() + size;
+					squares[i][j].setX(amountUnder);
+				} else if (squares[i][j].getY() + squares[i][j].getYVel() < 0) {
+					int amountUnder = squares[i][j].getY() + squares[i][j].getYVel() + size;
+					squares[i][j].setY(amountUnder);
+				}
 			}
 		}
 
@@ -433,7 +375,12 @@ public class Globe {
 
 				// pre emptive check for overlapping - before actual move. one step ahead.
 
-				if (groupMap[squares[i][j].getX() + (squares[i][j].getXVel())][squares[i][j].getY()
+				// so it's trying to access group map array index beyond the range - why. i
+				// thought the above would always handle these boundaries before it
+
+				if (groupMap[squares[i][j].getX() + (squares[i][j].getXVel())][squares[i][j].getY() // arrayIndexOutofBounds:
+																									// 152 , 150 , -2 ,
+																									// 150
 						+ (squares[i][j].getYVel())] > 0) {
 					if (groupMap[squares[i][j].getX() + (squares[i][j].getXVel())][squares[i][j].getY()
 							+ (squares[i][j].getYVel())] != squares[i][j].getGroup()) {
@@ -450,10 +397,7 @@ public class Globe {
 						// collision as below.
 						// this means we can changes the heights of all these squares, and their
 						// neighbours, and so on.
-
 						collisions.add(squares[i][j]);
-						// just make variable for i and j you fucking idiot. carry them over.
-
 					}
 				}
 
@@ -465,92 +409,41 @@ public class Globe {
 
 			// which groups are involved?
 			int g1 = squares[o][p].getGroup();
-			int g2 = groupMap[squares[o][p].getX()][squares[o][p].getY()];
+			int g2 = groupMap[squares[o][p].getX() + squares[o][p].getXVel()][squares[o][p].getY()
+					+ squares[o][p].getYVel()];
 
-			// get mass
+			// masses can simply equal number of squares
+			int mass1 = continents[g1].size();			
+			int mass2 = continents[g2].size();
+			
+			double nx1 = continents[g1].get(0).getXVel() * (mass1 - mass2)
+					+ 2 * (mass2 * continents[g2].get(0).getXVel());
+			double ny1 = continents[g1].get(0).getYVel() * (mass1 - mass2)
+					+ 2 * (mass2 * continents[g2].get(0).getYVel());
+			double nx2 = continents[g2].get(0).getXVel() * (mass2 - mass1)
+					+ 2 * (mass1 * continents[g1].get(0).getXVel());
+			double ny2 = continents[g2].get(0).getYVel() * (mass2 - mass1)
+					+ 2 * (mass1 * continents[g1].get(0).getYVel());
 
-			// MATHS ***********************************
-
-			// angles, momentum, combination events etc...
-
-			// relax, i have taken out the changes in direction.
-
-			// can use the resulting direction to add mountain formation in the direction of
-			// impact, away from the collision line, in angle.
-
-			// maybe in collisions I should let them overlap a bit. this might help make
-			// combinations look better..
-			// or
-			// for combinations, i can let them go into each other a little, and then when
-			// splitting them later, change their directions and ignore each other until
-			// free...
-
-			// BELOW USED:
-			// https://www.emanueleferonato.com/2007/08/19/managing-ball-vs-ball-collision-with-flash/
-
-			/*
-			 * int mass1 = continents[g1].size(); int mass2 = continents[g2].size();
-			 * 
-			 * double dX = continents[g1].get(0).getX() - continents[g2].get(0).getX();
-			 * double dY = continents[g1].get(0).getY() - continents[g2].get(0).getY();
-			 * 
-			 * double collisionAngle = Math.atan2(dY,dX);
-			 * System.out.println(collisionAngle);
-			 * 
-			 * double mag1 = Math.sqrt(continents[g1].get(0).getXVel() *
-			 * continents[g1].get(0).getXVel() + continents[g1].get(0).getYVel() *
-			 * continents[g1].get(0).getYVel()); double mag2 =
-			 * Math.sqrt(continents[g2].get(0).getXVel() * continents[g2].get(0).getXVel() +
-			 * continents[g2].get(0).getYVel() * continents[g2].get(0).getYVel());
-			 * 
-			 * double dir1 = Math.atan2(continents[g1].get(0).getYVel(),
-			 * continents[g1].get(0).getXVel()); double dir2 =
-			 * Math.atan2(continents[g2].get(0).getYVel(), continents[g2].get(0).getXVel());
-			 * 
-			 * double newXspeed1 = mag1 * Math.cos(dir1 - collisionAngle); double newYspeed1
-			 * = mag1 * Math.sin(dir1 - collisionAngle);
-			 * 
-			 * double newXspeed2 = mag2 * Math.cos(dir2 - collisionAngle); double newYspeed2
-			 * = mag2 * Math.sin(dir2 - collisionAngle);
-			 * 
-			 * double finXspeed1 = ((mass1 - mass2) * newXspeed1 + (mass2 + mass2) *
-			 * newXspeed2) / (mass1 + mass2); double finXspeed2 = ((mass1 + mass1) *
-			 * newXspeed1 + (mass2 - mass1) * newXspeed2) / (mass1 + mass2);
-			 * 
-			 * double finYspeed1 = newYspeed1; double finYspeed2 = newYspeed2;
-			 * 
-			 * 
-			 * double aX = Math.cos(collisionAngle) * finXspeed1 + Math.cos(collisionAngle +
-			 * Math.PI/2) * finYspeed1; System.out.println(aX); double aY =
-			 * Math.sin(collisionAngle) * finXspeed1 + Math.sin(collisionAngle + Math.PI/2)
-			 * * finYspeed1; System.out.println(aY); double bX = Math.cos(collisionAngle) *
-			 * finXspeed2 + Math.cos(collisionAngle + Math.PI/2) * finYspeed2;
-			 * System.out.println(bX); double bY = Math.sin(collisionAngle) * finXspeed2 +
-			 * Math.sin(collisionAngle + Math.PI/2) * finYspeed2; System.out.println(bY);
-			 * 
-			 * for (Square a : continents[g1]) { a.setXVel((int)Math.round(aX));
-			 * a.setYVel((int)Math.round(aY)); }
-			 * 
-			 * for (Square b : continents[g2]) { b.setXVel((int)Math.round(bX));
-			 * b.setYVel((int)Math.round(bY)); } }
-			 */
-
-			// ******************************************
-
-			int aXv = continents[g1].get(0).getXVel();
-			int aYv = continents[g1].get(0).getYVel();
-			int bXv = continents[g2].get(0).getXVel();
-			int bYv = continents[g2].get(0).getYVel();
+			nx1 = nx1 / (mass1 + mass2);
+			System.out.println(nx1);
+			ny1 = ny1 / (mass1 + mass2);
+			System.out.println(ny1);
+			nx2 = nx2 / (mass1 + mass2);
+			System.out.println(nx2);
+			ny2 = ny2 / (mass1 + mass2);
+			System.out.println(ny2);
 
 			for (Square a : continents[g1]) {
-				a.setXVel(-aXv);
-				a.setYVel(-aYv);
+				a.setXVel((int) nx1);
+				a.setYVel((int) ny1);
 			}
 
 			for (Square b : continents[g2]) {
-				b.setXVel(-bXv);
-				b.setYVel(-bYv);
+				b.setXVel((int) nx2);
+				b.setYVel((int) ny2);
 			}
+
 		}
 
 		// get those that overlapped
@@ -626,7 +519,11 @@ public class Globe {
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				// if (squares[i][j].getX() > 0 && squares[i][j].getY() > 0) {
-				heightMap[squares[i][j].getX()][squares[i][j].getY()] = squares[i][j].getHeight();
+				heightMap[squares[i][j].getX()][squares[i][j].getY()] = squares[i][j].getHeight(); // error here , in
+																									// many cases.
+																									// arrayIndexOutofBounds:
+																									// 150... 150, -2, -
+																									// 4
 
 				// }
 			}
@@ -644,15 +541,22 @@ public class Globe {
 
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				groupMap[squares[i][j].getX()][squares[i][j].getY()] = squares[i][j].getGroup();
+				groupMap[squares[i][j].getX()][squares[i][j].getY()] = squares[i][j].getGroup(); // index out of bounds
+																									// - 150 ???
 			}
 		}
 	}
 
 	// plot new positions on the group map, and loop through to see if any planned
 	// positions have continent in them.
-	public void moveOnGroupMap() {
-		// need to efficiently check these collisions...
+	public void displayGroupMap() {
+		for (int i = 0; i < size; i++) {
+			System.out.println();
+			for (int j = 0; j < size; j++) {
+				System.out.print(groupMap[i][j] + " ");
+
+			}
+		}
 	}
 
 	public int[][] getHeightMap() {
--- GlobeGUI.java
+++ GlobeGUI.java
@@ -34,8 +34,8 @@ public class GlobeGUI extends JFrame implements ActionListener{
 		//g.makeOtherContinent();
 		g.newNumbers(10,10,50, 1);
 		g.newNumbers(100,100,149, 2);
-		g.setVelocity(1, 5, 2);
-		g.setVelocity(2,1,4);
+		g.setVelocity(1, 1, 1);
+		g.setVelocity(2,-2,-2);
 		//g.plotToHeightMap();
 		
 		//g.generate(4);
